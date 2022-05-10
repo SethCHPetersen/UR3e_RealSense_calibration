465ede4ab77d056b91534cea37cf5a3191cc4fc9
SethCHPetersen <100106972+SethCHPetersen@users.noreply.github.com>
2022-05-09 23:52:55 -0500

    Delete python_code_for_calibration directory

--- python_code_for_calibration/build/auto_calibrate/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seth/python_code_for_calibration/src/auto_calibrate
-# Build directory: /home/seth/python_code_for_calibration/build/auto_calibrate
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
