d70999cbcfa7371e029831fdcd31cee76c2b0835
Seth Petersen <sethchp@gmail.com>
2022-05-09 23:59:35 -0500

    seccond push

---
+++ python_code_for_calibration/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/seth/python_code_for_calibration/src
+# Build directory: /home/seth/python_code_for_calibration/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("auto_calibrate")
---
+++ python_code_for_calibration/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/seth/python_code_for_calibration/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/seth/python_code_for_calibration/devel;/home/seth/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/seth/python_code_for_calibration/devel;/home/seth/catkin_ws/devel;/opt/ros/melodic' != '' else []
